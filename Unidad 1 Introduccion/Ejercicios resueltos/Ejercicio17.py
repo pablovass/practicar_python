302e6b58a2a7e44670417d159148b48abf5c96a2
Sebastardo <sebastardo@outlook.com>
2019-09-16 01:36:59 -0300

    Algunos ejercicios hechos

--- Unidad 1 Introduccion/Ejercicios resueltos/Ejercicio17.py
+++ Unidad 1 Introduccion/Ejercicios resueltos/Ejercicio17.py
@@ -4,17 +4,17 @@ o no primo. Nota: Ningun numero es divisible por un numero mayor a la raiz cuadr
 de si mismo
 '''
 import math
+
 def pedirNumero() -> int:
     return int(input("numero: "))
 
 
-def numeroPrimo(numero):
+def numeroPrimo(numero:int)->bool:
     i=2
     while i <math.sqrt(numero):
         if numero%i==0:
             return False
         i+=1
-        
     return True
 
 def main():
