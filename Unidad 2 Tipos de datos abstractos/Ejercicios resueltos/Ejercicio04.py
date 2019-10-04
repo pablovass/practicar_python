a34008b28ad280cd9cfcc4121ed147d80c2f8e4f
Sebastardo <sebastardo@outlook.com>
2019-10-03 21:22:03 -0300

    Mas ejercicios

--- Unidad 2 Tipos de datos abstractos/Ejercicios resueltos/Ejercicio04.py
+++ Unidad 2 Tipos de datos abstractos/Ejercicios resueltos/Ejercicio04.py
@@ -13,3 +13,54 @@ e) Una función que a partir de dos fechas obtenga la cantidad de días que hay 
 ellas.
 '''
 
+class Fecha:
+    def __init__(self, dia, mes, anio):
+        self.dia=dia
+        self.mes=mes
+        self.anio=anio
+        if(not self.fechaCorrecta()):
+            print("Fecha Erronea")
+            self.dia=1
+            self.mes=1
+            self.anio=2019
+
+    
+
+    def fechaCorrecta(self):
+        dias = [[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+                [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]]
+
+        if self.anio > 2019:
+            return False
+        if self.mes not in range(1, 13):
+            return False
+        if self.dia<1 or self.dia>dias[self.esBisiesto()][self.mes-1]:
+            return False
+        
+        return True
+
+
+    def esBisiesto(self):
+        return 1 if self.anio%4 == 0 and self.anio%100 != 0 or self.anio%400 == 0 else 0
+
+    def anterior(self):
+        pass
+
+    def siguiente(self):
+        pass
+
+    def sumaDias(self,dias):
+        pass
+    
+    def restaDias(self,dias):
+        pass
+
+    def diasEntre(self, fecha2):
+        pass
+
+    def __str__(self):
+        return f'{self.dia}/{self.mes}/{self.anio}'
+
+
+a = Fecha(2,31,1980)
+print(a)
