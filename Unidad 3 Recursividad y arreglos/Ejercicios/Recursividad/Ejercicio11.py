1cbabc5acc2d0eaccbea4a4ca5e5d38b3eb590b3
Sebastardo <sebastardo@outlook.com>
2019-10-23 22:46:42 -0300

    Mas recursividad!

--- Unidad 3 Recursividad y arreglos/Ejercicios/Recursividad/Ejercicio11.py
+++ Unidad 3 Recursividad y arreglos/Ejercicios/Recursividad/Ejercicio11.py
@@ -4,8 +4,11 @@ forma inversa, es decir, que muestre por pantalla, al finalizar en ingreso de lo
 numeros, desde el ultimo hasta el primero. Implementarlo con una funcion recursiva
 '''
 
-def inversador():
-    pass
+def inversador(numeros):
+    if len(numeros) == 1:
+        return numeros
+    return inversador(numeros[1:len(numeros)]) + numeros[0:1]
 
 if __name__ == '__main__':
-    pass
+    lista = [1,2,3,4]
+    print(inversador(lista))
