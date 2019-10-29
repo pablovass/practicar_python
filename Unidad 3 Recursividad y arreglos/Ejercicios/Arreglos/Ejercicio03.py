0b4a8fee466f9c10cff2175aaa4de0151cd5aea6
Sebastian Rossi <35445409+sebastardo@users.noreply.github.com>
2019-10-29 11:45:13 -0300

    Update and rename Ejercicio03-.py to Ejercicio03.py

--- Unidad 3 Recursividad y arreglos/Ejercicios/Arreglos/Ejercicio03.py
+++ Unidad 3 Recursividad y arreglos/Ejercicios/Arreglos/Ejercicio03.py
@@ -5,3 +5,18 @@ tercero y así sucesivamente. El último pasa a ser el primero. Guardar los nume
 ingresados en un arreglo en el orden original y luego correrlos dentro del mismo
 arreglo. Mostrarlos antes y despues por pantalla.
 '''
+
+def coso(lista):
+    aux = lista[0]
+    posicion = 1
+    while posicion<len(lista):
+        aux2 = lista[posicion]
+        lista[posicion] = aux
+        aux = aux2
+        posicion+=1
+    lista[0]=aux
+    return lista
+  
+  
+  if __name__ == '__main__':
+    print(coso([1,2,3,4]))
